83406418138dd7bf1899c2a3ddc4ad8a019bc439
hophiphip <nighop@yandex.ru>
2022-04-09 01:05:43 +0400

    Config updates

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+git init
+git checkout -b master
+git add -A
+git commit -m 'deploy'
+
+# deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:hophiphip/wep.git main:gh-pages
+
+cd -
---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+
+const basePath = '/wep/'
+
+export default defineConfig({
+    optimizeDeps: {
+        esbuildOptions: {
+            target: 'es2020', // https://github.com/vitejs/vite/issues/7364
+        },
+    },
+
+    build: {
+        rollupOptions: {
+            output: {
+                assetFileNames: `assets/[name].[ext]` // https://github.com/vitejs/vite/issues/378
+            }
+        },
+        target: 'es2020', // https://github.com/vitejs/vite/issues/7364
+    },
+
+    base: basePath,
+})
