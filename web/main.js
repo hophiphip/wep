8638d858dc8536f3b9928786df2693f7b26e047b
hophiphip <nighop@yandex.ru>
2022-04-09 22:31:44 +0400

    More examples & ui updates

--- web/main.js
+++ web/main.js
@@ -1,8 +1,10 @@
 import renderPlain from '../src/plain/index.js';
 import renderTriangle from '../src/triangle/index.js';
+import renderUniforms from '../src/uniforms/index.js';
 
-const plainColorCanvas = document.getElementById('plain-color');
+const plainColorCanvas = document.getElementById('plain');
 const triangleCanvas = document.getElementById('triangle');
+const uniformsCanvas = document.getElementById('uniforms');
 
 const onWebGlContextPlainColorLost = function () {
     alert('WebGL plain color context lost. You will need to reload the page.'); 
@@ -12,5 +14,10 @@ const onWebGlContextTriangleLost = function () {
     alert('WebGL triangle color context lost. You will need to reload the page');
 }
 
+const onWebGlContextUniformsLost = function () {
+    alert('WebGL uniforms context lost. You will need to reload the page');
+}
+
 renderPlain(plainColorCanvas, onWebGlContextPlainColorLost);
 renderTriangle(triangleCanvas, onWebGlContextTriangleLost);
+renderUniforms(uniformsCanvas, onWebGlContextUniformsLost);
