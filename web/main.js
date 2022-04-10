dbd8d20f28d7436b2a62f7a9f844d5d4d81a3311
hophiphip <nighop@yandex.ru>
2022-04-10 20:01:37 +0400

    Added Alpine

--- web/main.js
+++ web/main.js
@@ -1,23 +1,39 @@
+import Alpine from 'alpinejs';
+
 import renderPlain from '../src/plain/index.js';
 import renderTriangle from '../src/triangle/index.js';
 import renderUniforms from '../src/uniforms/index.js';
 
-const plainColorCanvas = document.getElementById('plain');
-const triangleCanvas = document.getElementById('triangle');
-const uniformsCanvas = document.getElementById('uniforms');
+window.Alpine = Alpine; 
 
-const onWebGlContextPlainColorLost = function () {
-    alert('WebGL plain color context lost. You will need to reload the page.'); 
-}
+const onWebGlContextPlainColorLost = function () { alert('WebGL plain color context lost. You will need to reload the page.');   }
+const onWebGlContextTriangleLost   = function () { alert('WebGL triangle color context lost. You will need to reload the page'); }
+const onWebGlContextUniformsLost   = function () { alert('WebGL uniforms context lost. You will need to reload the page');       }
 
-const onWebGlContextTriangleLost = function () {
-    alert('WebGL triangle color context lost. You will need to reload the page');
-}
+Alpine.data('main', () => ({
+    renderers: [
+        { 
+            id: 'plain', 
+            title: 'Hello Window',
+            reference: 'https://learnopengl.com/Getting-started/Hello-Window',
+            renderer: renderPlain, 
+            onContextLost: onWebGlContextPlainColorLost,
+        }, 
+        { 
+            id: 'triangle', 
+            title: 'Hello Triangle',
+            reference: 'https://learnopengl.com/Getting-started/Hello-Triangle',
+            renderer: renderTriangle,
+            onContextLost: onWebGlContextTriangleLost,  
+        }, 
+        { 
+            id: 'uniforms', 
+            title: 'Uniforms',
+            reference: 'https://learnopengl.com/Getting-started/Shaders', 
+            renderer: renderUniforms, 
+            onContextLost: onWebGlContextUniformsLost,  
+        }, 
+    ],
+}));
 
-const onWebGlContextUniformsLost = function () {
-    alert('WebGL uniforms context lost. You will need to reload the page');
-}
-
-renderPlain(plainColorCanvas, onWebGlContextPlainColorLost);
-renderTriangle(triangleCanvas, onWebGlContextTriangleLost);
-renderUniforms(uniformsCanvas, onWebGlContextUniformsLost);
+Alpine.start();
